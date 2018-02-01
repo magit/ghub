b267bb6c55b0c05aec4d3fe0e9385ab0e1139463
Jonas Bernoulli <jonas@bernoul.li>
2018-02-01 13:14:14 +0100

    Fix typos and improve wording

--- ghub.el
+++ ghub.el
@@ -198,7 +198,8 @@ Use HEADERS for those rare resources that require that the data
   mentions it explicitly.
 
 If UNPAGINATE is non-nil, then make multiple requests if necessary
-  to get all items at RESOURCE.
+  to get all items at RESOURCE.  For forward-compatibility avoid
+  using using a function as value.
 If NOERROR is non-nil, then do not raise an error if the request
   fails and return nil instead.
 If READER is non-nil, then it is used to read and return from the
