4831933da059ee084a16016558b9ccd8c581a8ff
Jonas Bernoulli <jonas@bernoul.li>
2018-04-19 12:08:02 +0200

    glab.el: Fix commentary

--- glab.el
+++ glab.el
@@ -23,7 +23,7 @@
 
 ;;; Commentary:
 
-;; Ghub is a library that provides basic support for using the Github API
+;; Glab is a library that provides basic support for using the Gitlab API
 ;; from Emacs packages.  It abstracts access to API resources using only
 ;; a handful of functions that are not resource-specific.
 
