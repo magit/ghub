4d7a0921067cbf3c80d1cb682095bb3a90db55be
Jonas Bernoulli <jonas@bernoul.li>
2018-09-10 23:32:23 +0200

    Add ghub-pkg.el
    The Package-Requires line would other wise get to long soon.
    Use "0" as version because Melpa ignores this anyway and we
    haven't been using Package-Version either.

---
+++ ghub-pkg.el
@@ -0,0 +1,5 @@
+(define-package "ghub" "0"
+  "Minuscule client libraries for Git forge APIs."
+  '((emacs "24.4")
+    (let-alist "1.0.5")
+    ))
--- ghub.el
+++ ghub.el
@@ -5,7 +5,6 @@
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/magit/ghub
 ;; Keywords: tools
-;; Package-Requires: ((emacs "24.4") (let-alist "1.0.5"))
 
 ;; This file is not part of GNU Emacs.
 
