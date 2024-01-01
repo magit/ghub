ba56fe223fbabab89fee577a1d3b0704d7c4a0df
Jonas Bernoulli <jonas@bernoul.li>
2024-01-01 23:17:10 +0100

    Bump copyright years

--- lisp/gogs.el
+++ lisp/gogs.el
@@ -1,6 +1,6 @@
 ;;; gogs.el --- Client library for the Gogs API  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2016-2023 Jonas Bernoulli
+;; Copyright (C) 2016-2024 Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/magit/ghub
