56c76725348da9d7abab88cec64a06505c67abe9
Jonas Bernoulli <jonas@bernoul.li>
2024-07-13 21:18:01 +0200

    Bump compat dependency

--- ghub-pkg.el
+++ ghub-pkg.el
@@ -1,6 +1,6 @@
 (define-package "ghub" "3.6.0"
   "Client libraries for Git forge APIs."
   '((emacs     "26.3")
-    (compat    "29.1.4.5")
+    (compat    "30.0.0.0")
     (let-alist "1.0.6")
     (treepy    "0.1.2")))
