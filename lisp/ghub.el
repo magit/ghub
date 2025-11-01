447cb51fa7d19e1fb3844acdd2c540be04299ffb
Jonas Bernoulli <jonas@bernoul.li>
2025-11-01 22:26:59 +0100

    Release version 5.0.2

--- lisp/ghub.el
+++ lisp/ghub.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/magit/ghub
 ;; Keywords: tools
 
-;; Package-Version: 5.0.1
+;; Package-Version: 5.0.2
 ;; Package-Requires: (
 ;;     (emacs   "29.1")
 ;;     (compat  "30.1")
